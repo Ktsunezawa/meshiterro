9ba25a1810bcc9ae6235463023bffda69d7e7431
Ktsunezawa <rapurasnohako@gmail.com>
2021-01-12 04:48:10 +0000

    Finish 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '32aa459fd210f1ec707e1983769a994eab852836547733bb8bfb7e573ef3d4524a8a86a7f5f6f0f4b78ca360a58504183e695e3b51be181c2db54ec8eb195927'
